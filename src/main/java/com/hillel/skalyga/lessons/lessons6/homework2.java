e8fadf86a61653ec77201b15375d6cc2e99d14a6
Ivan Skalyga <vanya729@ukr.net>
2023-02-18 23:34:32 +0200

    lessons6 homework2 shuttle

---
+++ src/main/java/com/hillel/skalyga/lessons/lessons6/homework2.java
@@ -0,0 +1,21 @@
+package com.hillel.skalyga.lessons.lessons6;
+
+public class homework2 {
+    public static void main(String[] args) {
+
+        int counter = 0;
+        for (int i = 1; i <= 200; i++) {
+            int secondNum = (i/10)%10;
+            if (i / 10 == 4 || i / 10 == 9 || i % 10 == 4 || i % 10 == 9 || secondNum==4 || secondNum==9) {
+                continue;
+            }
+            System.out.println("shuttle number = " + i);
+            counter++;
+            if (counter==100){
+                break;
+            }
+        }
+
+        System.out.println("count shuttle = " + counter);
+    }
+}
