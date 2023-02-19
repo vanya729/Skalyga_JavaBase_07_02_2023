2881094d0bd5b6b26ae37ee21cd6e180bcaf5922
Ivan Skalyga <vanya729@ukr.net>
2023-02-19 17:41:58 +0200

    lessons6 homework1

---
+++ src/main/java/com/hillel/skalyga/lessons/lessons6/homework.java
@@ -0,0 +1,43 @@
+package com.hillel.skalyga.lessons.lessons6;
+
+import java.util.Scanner;
+
+public class homework {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int userNum =0;
+        int randNumber = (int) (Math.random() * 11);
+
+        while (true) {
+            if (scanner.hasNextInt() ) {
+                userNum = scanner.nextInt();
+                break;
+            } else {
+                System.out.println("Wrong data!!!");
+                scanner.nextLine();
+//                continue;
+            }
+
+        }
+        for (int popitok = 3; popitok >0 ; --popitok) {
+
+            if (popitok==1) {
+                System.out.println("You lose( , correct answer =" + randNumber);
+                break;
+            }
+
+            if (userNum>randNumber){
+                System.out.println("Your number is less than expected, number of attempts:" + popitok);
+                userNum=scanner.nextInt();
+            } else if (userNum<randNumber){
+                System.out.println("Your number is higher, number of attempts:" + popitok);
+                userNum=scanner.nextInt();
+            } else if (userNum==randNumber){
+                System.out.println("You WIN!!!");
+                break;
+            }
+
+        }
+    }
+}
