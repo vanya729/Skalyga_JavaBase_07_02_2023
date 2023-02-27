48c0b10d039171e269b530c3ee5b3e76531909e0
Ivan Skalyga <vanya729@ukr.net>
2023-02-27 11:12:19 +0200

    lessons8 homework2

---
+++ src/main/java/com/hillel/skalyga/lessons/lessons8/homework2.java
@@ -0,0 +1,52 @@
+package com.hillel.skalyga.lessons.lessons8;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class homework2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int m=0,n=0;
+
+        System.out.println("Input number(m,n): ");
+        if (scanner.hasNextInt()) {
+            m =scanner.nextInt();
+            n =scanner.nextInt();
+            System.out.println("m= "+m);
+            System.out.println("n= "+n);
+        } else {
+            System.out.println("Input ONLY NUMBER");
+            System.exit(0);
+        }
+
+        int[][] array1 = new int[m][n];
+        int[][] array2 = new int[n][m];
+
+        for (int i = 0; i <m ; i++) {
+            for (int j = 0; j < n; j++) {
+                array1[i][j] = (int) ((Math.random() * 10));
+                System.out.print(array1[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                array2[i][j]= array1[j][i];
+                System.out.print(array2[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+/*
+Транспонуйте матрицю M x N. Розміри M та N задаються з консолі.
+Після введення повинні автоматично створюватися два масиви розміром M x N перший та N x M другий.
+ */
+    }
+}
