6675ae4b3a4c68bcf6c299ed43055f1ae35d483c
Ivan Skalyga <vanya729@ukr.net>
2023-03-13 17:59:26 +0200

    lessons8 corrected matrix

--- src/main/java/com/hillel/skalyga/lessons/lessons8/Homework2.java
+++ src/main/java/com/hillel/skalyga/lessons/lessons8/Homework2.java
@@ -1,58 +1,30 @@
 package com.hillel.skalyga.lessons.lessons8;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Homework2 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int m=0,n=0;
+        int n = getCorrectNum(scanner);
+        int m = getCorrectNum(scanner);
 
-        while (true) {
-            System.out.println("Input number(m,n): ");
-            if (scanner.hasNextInt()) {
-                m = scanner.nextInt();
-                n = scanner.nextInt();
-                System.out.println("m= " + m);
-                System.out.println("n= " + n);
-                if (m < 0 ) {
-                    System.out.println("Enter positive m");
-                    scanner.nextLine();
-                    continue;
-                } if ( n < 0) {
-                    System.out.println("Enter positive n");
-                    scanner.nextLine();
-                    continue;
-                }
-                else {
-                    break;
-                }
-            } else {
-                System.out.println("Input ONLY NUMBER");
-                scanner.nextLine();
-
-            }
-        }
-        int[][] array1 = new int[m][n];
-        int[][] array2 = new int[n][m];
-
-        for (int i = 0; i <m ; i++) {
-            for (int j = 0; j < n; j++) {
-                array1[i][j] = (int) ((Math.random() * 10));
-                System.out.print(array1[i][j] + " ");
+        int [][] arr1 = new int[n][m];
+        int [][] arr2 = new int[m][n];
+        for (int i = 0; i < arr1.length ; i++) {
+            for (int j = 0; j < arr1[i].length ; j++) {
+                arr1[i][j]= (int) (Math.random()*101);
+                System.out.print(arr1[i][j] + " ");
             }
             System.out.println();
         }
 
-
         System.out.println();
-
-
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                array2[i][j]= array1[j][i];
-                System.out.print(array2[i][j]+ " ");
+        for (int i = 0; i < arr2.length ; i++) {
+            for (int j = 0; j < arr2[i].length ; j++) {
+                arr2[i][j]= arr1[j][i];
+                System.out.print(arr2[i][j] + " ");
             }
             System.out.println();
         }
@@ -62,4 +34,25 @@ public class Homework2 {
 Після введення повинні автоматично створюватися два масиви розміром M x N перший та N x M другий.
  */
     }
+
+    static int getCorrectNum(Scanner scanner) {
+        int num = 0;
+        System.out.println("Enter n, m");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                num = scanner.nextInt();
+            if (num < 0) {
+                System.out.println("Enter positive number");
+            } else {
+                break;
+            }
+            scanner.nextLine();
+        }else{
+            System.out.println("Error");
+            scanner.nextLine();
+        }
+    }
+        return num;
+    }
 }
+
